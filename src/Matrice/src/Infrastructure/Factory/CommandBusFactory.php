8db56e505c782eaceee7748bee166ac5e129af29
demijohn <mbilcik123@gmail.com>
2020-03-15 18:04:10 +0100

    Add transaction middleware into command bus
    Add locking middleware into command bus

--- src/Matrice/src/Infrastructure/Factory/CommandBusFactory.php
+++ src/Matrice/src/Infrastructure/Factory/CommandBusFactory.php
@@ -5,9 +5,11 @@ namespace Matrice\Infrastructure\Factory;
 
 use League\Tactician\CommandBus;
 use League\Tactician\Container\ContainerLocator;
+use League\Tactician\Doctrine\ORM\TransactionMiddleware;
 use League\Tactician\Handler\CommandHandlerMiddleware;
 use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
 use League\Tactician\Handler\MethodNameInflector\HandleInflector;
+use League\Tactician\Plugins\LockingMiddleware;
 use Psr\Container\ContainerInterface;
 
 final class CommandBusFactory
@@ -31,7 +33,15 @@ final class CommandBusFactory
             new HandleInflector(),
         );
 
+        $lockingMiddleware = new LockingMiddleware();
+
+        $transactionMiddleware = new TransactionMiddleware(
+            $container->get('doctrine.entity_manager.orm_default'),
+        );
+
         return new CommandBus([
+            $lockingMiddleware,
+            $transactionMiddleware,
             $handlerMiddleware,
         ]);
     }
